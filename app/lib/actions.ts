cb749ea855b6a15aec9f5826b9911ad1f538f636
Sergey Grigorash <sergey_grigorash@epam.com>
2025-05-30 13:43:56 +0500

    14-2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -66,13 +66,22 @@ export async function createInvoice(prevState: State, formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State,  formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Update Invoice.',
+        };
+    }
+
+// Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
 
     try {
@@ -82,8 +91,9 @@ export async function updateInvoice(id: string, formData: FormData) {
     WHERE id = ${id}
   `;
     } catch (error) {
-        // We'll log the error to the console for now
-        console.error(error);
+        return {
+            message: 'Database Error: Failed to Update Invoice.',
+        };
     }
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
